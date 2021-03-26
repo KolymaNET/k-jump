9b0bfc77567dc23f9d341eeb41cf143d2d7e8a49
Yuri Kuznetsov <60663886+kuz-sysadmin@users.noreply.github.com>
2021-03-26 19:26:03 -0400

    Create jump.php

---
+++ jump.php
@@ -0,0 +1,73 @@
+<?php
+define('SQLHOST', '');
+define('SQLUSER', '');
+define('SQLPASS', '');
+define('SQLDB', '');
+define('SQLTABLE', 'ref');
+define('REMOTE_ADDR', $_SERVER['REMOTE_ADDR']); // change for cloudflare
+define('_RETURN', 'index.html');
+
+// URL match
+$URL = htmlspecialchars($_SERVER['QUERY_STRING']);
+if (!preg_match('/^[a-zA-Z]+:\/\//i', $URL)) $URL = '//'.$URL;
+$URL = strtolower($URL);
+$msg = $URL ? 'Are you sure you want to leave?' : 'Invalid URL.';
+
+// proxy
+$proxy = '';
+if ($_SERVER['HTTP_CLIENT_IP']??false)
+	$proxy = $_SERVER['HTTP_CLIENT_IP'];
+else if ($_SERVER['HTTP_X_FORWARDED_FOR']??false)
+	$proxy = $_SERVER['HTTP_X_FORWARDED_FOR'];
+if ($proxy==REMOTE_ADDR
+ || !filter_var($proxy, FILTER_VALIDATE_IP))
+	$proxy = '';
+
+
+// page
+?><!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>K! Jump</title>
+		<meta name="robots" content="nofollow,noarchive" />
+		<script>
+function l() {
+	var link = document.getElementById("linkto");
+	link.focus();
+}
+		</script>
+<style>
+    body {
+    font-family: Mona, 'MS PGothic' !important;
+    word-wrap: break-word;
+    line-height: 1;
+    font-size: 12pt;
+}
+</style>
+	</head>
+	<body onload="l();" link="#0000ee" vlink="#0000EE" text="#000" bgcolor="#ffffff" alink="#FF0000">
+        <h1>★K! Jump★</h1>
+        <hr>
+<center>
+<p><font color="#F00" size="6"><b>■WARNING■</b></font></p>
+
+		<?php if($URL){ ?><a rel="noopener noreferrer" href="<?=$URL?>" id="linkto"><?=$URL?></a><?php } ?>
+		<p>
+			<font size="4" color="#F00"><b><?=$msg?></b></font>
+            
+            <p>[<a href="<?=$_SERVER['HTTP_REFERER']??_RETURN?>" onclick="event.preventDefault();history.go(-1);">Back</a>] [<a rel="noopener noreferrer" href="<?=$URL?>" id="linkto">Continue</a>]</p>
+		</p>
+</center>
+        <hr>
+		<p align="RIGHT"><font color="#080"><b>K! Jump ★</b></font></p>
+	</body>
+</html>
+<?php
+exit;
+
+function error($err) { global $msg, $URL;
+	$msg = $err;
+	$URL = '';
+}
+?>
